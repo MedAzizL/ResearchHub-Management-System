42432331bd27e88b55f719e8399de9e90bea9cf9
mazenhaouari40 <mazenhaouari97@gmail.com>
2025-02-24 21:54:06 +0100

    first commit

---
+++ src/test/java/com/example/ResearchHub/ResearchHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ResearchHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
