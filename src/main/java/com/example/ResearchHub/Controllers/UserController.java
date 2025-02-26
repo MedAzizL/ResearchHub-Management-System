559c37e227b8f250893fea0e228c9ec6b9f3f861
Yazid Dhouioui <88597706+YazidDh@users.noreply.github.com>
2025-02-26 22:55:43 +0100

    add all users retrieval endpoint

--- src/main/java/com/example/ResearchHub/Controllers/UserController.java
+++ src/main/java/com/example/ResearchHub/Controllers/UserController.java
@@ -1,15 +1,15 @@
 package com.example.ResearchHub.Controllers;
 
 import com.example.ResearchHub.Dto.UserCreateRequest;
+import com.example.ResearchHub.Entities.User;
 import com.example.ResearchHub.Services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
@@ -22,5 +22,10 @@ public class UserController {
     public ResponseEntity<String> createUser(@RequestBody UserCreateRequest userCreateRequest) {
         userService.createUser(userCreateRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body("User created successfully");
+    }
+
+    @GetMapping
+    public  List<User> getAllUsers() {
+        return userService.getAllUsers();
     }
 }
--- src/main/java/com/example/ResearchHub/Services/UserService.java
+++ src/main/java/com/example/ResearchHub/Services/UserService.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -29,7 +31,9 @@ public class UserService {
         userRepository.save(user);
     }
 
-    
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 
 
 }
