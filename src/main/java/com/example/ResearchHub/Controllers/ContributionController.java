807dd465d42f4a1f556377c6274f836f2b8873d2
mazenhaouari40 <mazenhaouari97@gmail.com>
2025-02-26 16:29:32 +0100

    adding autowired

--- src/main/java/com/example/ResearchHub/Controllers/ContributionController.java
+++ src/main/java/com/example/ResearchHub/Controllers/ContributionController.java
@@ -1,9 +1,13 @@
 package com.example.ResearchHub.Controllers;
 
+import com.example.ResearchHub.Services.ContributionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/contribution")
 public class ContributionController {
+    @Autowired
+    private ContributionService service;
 }
