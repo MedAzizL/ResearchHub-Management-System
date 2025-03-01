d50d1776e00473bac769ea2f94efaf3eff7d53a0
3zayez <mohamedaziz.laifi@etudiant-isi.utm.tn>
2025-03-01 23:52:21 +0100

    Domain  crud starter

---
+++ src/main/java/com/example/ResearchHub/Entities/Domain.java
@@ -0,0 +1,15 @@
+package com.example.ResearchHub.Entities;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Table(name = "domains")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder  // add builder
+public class Domain {
+
+}
