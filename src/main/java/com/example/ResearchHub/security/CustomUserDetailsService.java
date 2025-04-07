297646d57a1a8809dc4cf858e78ee0ddbe27eb8f
mazenhaouari40 <mazenhaouari97@gmail.com>
2025-04-07 21:17:54 +0200

    token configuration

--- src/main/java/com/example/ResearchHub/security/CustomUserDetailsService.java
+++ src/main/java/com/example/ResearchHub/security/CustomUserDetailsService.java
@@ -24,7 +24,6 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email).orElseThrow(()-> new UsernameNotFoundException("email not found"));
         return  new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(), Collections.emptyList());
-
     }
 
 }
